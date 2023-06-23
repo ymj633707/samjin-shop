326671656a0fa9ceb6f59626fc12d6882866527e
miji_YOON <ymj6337@gmail.com>
2023-06-23 11:37:42 +0900

    ver05

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ function App() {
      
      <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand onClick={() => {navigate('/samjin-shop')}}>Samjin_Shop</Navbar.Brand> 
+          <Navbar.Brand onClick={() => {navigate('/')}}>Samjin_Shop</Navbar.Brand> 
           <Nav className="me-auto">
-            <Nav.Link onClick={() => {navigate('/samjin-shop')}}>Home</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/')}}>Home</Nav.Link>
             <Nav.Link onClick={() => {navigate('/about')}}>About</Nav.Link>
             <Nav.Link onClick={() => {navigate('/about/info')}}>Information</Nav.Link>
             <Nav.Link onClick={() => {navigate('/cart')}}>Cart</Nav.Link>
@@ -37,7 +37,7 @@ function App() {
 
       <Routes>
 
-        <Route path='samjin-shop' element={
+        <Route path='/' element={
 
           <Container>
             <img src={process.env.PUBLIC_URL + './images/visual_main_01.jpg' } alt='visual_main' style={{width:1240, height:'auto', margin:'0 auto', marginBottom:50 }}/>
