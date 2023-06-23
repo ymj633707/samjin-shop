bcdde3447f15249504a48bf2d4cc825c607551a8
miji_YOON <ymj6337@gmail.com>
2023-06-23 11:20:20 +0900

    ver_02

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( //여기서 만약 오류 뜰경우 strictMode를 삭제할것
   <React.StrictMode>
       <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/samjin-shop'>
         <App />
       </BrowserRouter>
     </Provider>
